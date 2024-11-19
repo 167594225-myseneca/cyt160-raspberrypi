eab4aabe1a6b7c9f4cfef567dec14c5b78949722
167594225-myseneca <167594225-myseneca@email>
2024-11-19 18:24:33 +0000

    Committing cyt160-raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-4a57/tml_system_step_4_kafka_preprocess_dag-cybersecuritywithprivategpt-4a57.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-4a57/tml_system_step_4_kafka_preprocess_dag-cybersecuritywithprivategpt-4a57.py
@@ -37,7 +37,7 @@ default_args = {
   'usemysql' : '1', # do not modify
   'streamstojoin' : '', # leave blank
   'identifier' : 'Detect potential cyber attacks and monitor network', # <<< ** Change as needed
-  'preprocesstypes' : 'preprocesstypes' : 'min,max,trend,anomprob,variance,avg', # <<< **** MAIN PREPROCESS TYPES CHNAGE AS NEEDED refer to https://tml-readthedocs.readthedocs.io/en/latest/
+  'preprocesstypes' : 'min,max,trend,anomprob,variance,avg', # <<< **** MAIN PREPROCESS TYPES CHNAGE AS NEEDED refer to https://tml-readthedocs.readthedocs.io/en/latest/
   'pathtotmlattrs' : 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a', # Change as needed     
   'jsoncriteria' :  'uid=hostName,filter:allrecords~
 subtopics=hostName,hostName,hostName~
